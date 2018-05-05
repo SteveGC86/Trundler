8e3276348b705cb6691e41deb848c7731eeeae2d
Steven Christenson <stevenchristenson@Stevens-MacBook-Air.local>
2018-05-05 15:03:11 +1000

    Static pages routes, views and links created

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/about'
   get 'pages/contact'
+
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+
   resources :profiles
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
